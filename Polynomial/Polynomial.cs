c7c2d3dc59f5e4a23a4040e6e631880caa446e83
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-24 23:15:07 +0300

    +polinomial+ test folder

---
+++ Polynomial/Polynomial.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynomial
+{
+    public sealed class Polynomial
+    {
+        double[] coefficients;
+        public Polynomial()
+        {
+            this.coefficients = new double[1];
+        }
+
+        public Polynomial(params double[] coefficients)
+        {
+            this.coefficients = coefficients;
+        }
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            int degree  = coefficients.Length-1;
+            foreach (var item in coefficients)
+            {
+                if (item!=0)
+                {
+                    str.Append($"{item}*X^{degree}+");
+                }
+                degree--;
+            }
+            return str.ToString().Substring(0,str.Length-1);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var temp = obj as Polynomial;
+            for (int i = 0; i < this.coefficients.Length; i++)
+            {
+                if (this.coefficients[i]!=temp.coefficients[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public override int GetHashCode()
+        {
+            int hash=0;
+            foreach (var item in coefficients)
+            {
+                hash ^= item.GetHashCode();
+            }            
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ PolynomialTests/PolynomialTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+namespace PolynomialTests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [Test]
+        public void PolynomialToString()
+        {
+            Polynomial.Polynomial myPolinomial = new Polynomial.Polynomial(5,0,3,2,1);
+            Assert.AreEqual("5*X^4+3*X^2+2*X^1+1*X^0", myPolinomial.ToString());
+        }
+    }
+}
