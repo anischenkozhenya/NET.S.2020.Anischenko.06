33d89b68da80237d6f779ad84c42d35d21d16340
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2020-03-25 23:05:42 +0300

    +polynomial +polynomialTests sum tostr

--- Polynomial/Polynomial.cs
+++ Polynomial/Polynomial.cs
@@ -1,19 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Polynomial
 {
     public sealed class Polynomial
     {
-        double[] coefficients;
+        private double[] coefficients;
         public Polynomial()
         {
             this.coefficients = new double[1];
         }
-
+        public Polynomial(int maxDegree)
+        {
+            coefficients = new double[maxDegree];
+        }
         public Polynomial(params double[] coefficients)
         {
             this.coefficients = coefficients;
@@ -21,24 +21,57 @@ namespace Polynomial
         public override string ToString()
         {
             StringBuilder str = new StringBuilder();
-            int degree  = coefficients.Length-1;
+            int degree = coefficients.Length - 1;
             foreach (var item in coefficients)
             {
-                if (item!=0)
+                if (item != 0)
                 {
-                    str.Append($"{item}*X^{degree}+");
+                    if (item > 0)
+                    {
+                        if (degree == 1)
+                        {
+                            str.Append($"+{Math.Abs(item)}X");
+                        }
+                        else if (degree == 0)
+                        {
+                            str.Append($"+{Math.Abs(item)}");
+                        }
+                        else
+                        {
+                            str.Append($"+{Math.Abs(item)}X^{degree}");
+                        }
+                    }
+                    else
+                    {
+                        if (degree == 1)
+                        {
+                            str.Append($"+{Math.Abs(item)}X");
+                        }
+                        else if (degree == 0)
+                        {
+                            str.Append($"+{Math.Abs(item)}");
+                        }
+                        else
+                        {
+                            str.Append($"-{Math.Abs(item)}X^{degree}");
+                        }
+                    }
                 }
                 degree--;
             }
-            return str.ToString().Substring(0,str.Length-1);
+            string result = str.ToString();
+            if (result.StartsWith("+"))
+            {
+                result = result.Substring(1);
+            }
+            return result;
         }
-
         public override bool Equals(object obj)
         {
             var temp = obj as Polynomial;
             for (int i = 0; i < this.coefficients.Length; i++)
             {
-                if (this.coefficients[i]!=temp.coefficients[i])
+                if (this.coefficients[i] != temp.coefficients[i])
                 {
                     return false;
                 }
@@ -47,12 +80,92 @@ namespace Polynomial
         }
         public override int GetHashCode()
         {
-            int hash=0;
+            int hash = 0;
             foreach (var item in coefficients)
             {
                 hash ^= item.GetHashCode();
-            }            
+            }
             return base.GetHashCode();
+        }
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= coefficients.Length)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return coefficients[index];
+            }
+            set
+            {
+                if (index < 0 || index >= coefficients.Length)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                coefficients[index] = value;
+            }
+        }
+        public static Polynomial operator +(Polynomial first, Polynomial second)
+        {
+            var maxDegree = 0;
+            var Length = 0;
+            Polynomial result;
+            Polynomial min;
+            if (first.coefficients.Length <= second.coefficients.Length)
+            {
+                maxDegree = first.coefficients.Length;
+                Length = second.coefficients.Length;
+                result = second.MemberwiseClone() as Polynomial;
+                min = first;
+            }
+            else
+            {
+                maxDegree = second.coefficients.Length;
+                Length = first.coefficients.Length;
+                result = first.MemberwiseClone() as Polynomial;
+                min = second;
+            }
+
+            for (int i = Length - maxDegree, j = 0; i < Length; i++, j++)
+            {
+                result[i] += min[j];
+            }
+            return result;
+        }
+        public static Polynomial operator -(Polynomial first, Polynomial second)
+        {
+            return first + (-1) * second;
+        }
+        public static Polynomial operator *(double number, Polynomial polynomial)
+        {
+            var result = polynomial.MemberwiseClone() as Polynomial;
+            for (int i = 0; i < polynomial.coefficients.Length; i++)
+            {
+                polynomial[i] *= number;
+            }
+            return result;
+        }
+        public static Polynomial operator *(Polynomial polynomial, double number)
+        {
+            var result = polynomial.MemberwiseClone() as Polynomial;
+            for (int i = 0; i < polynomial.coefficients.Length; i++)
+            {
+                polynomial[i] *= number;
+            }
+            return result;
+        }
+        public static Polynomial operator *(Polynomial first, Polynomial second)
+        {
+            var result = new Polynomial(first.coefficients.Length + second.coefficients.Length - 1);
+            for (int i = 0; i < first.coefficients.Length; i++)
+            {
+                for (int j = 0; j < second.coefficients.Length; j++)
+                {
+                    result.coefficients[i + j] += (first.coefficients[i] * second.coefficients[j]);
+                }
+            }
+            return result;
         }
     }
 }
--- PolynomialTests/PolynomialTests.cs
+++ PolynomialTests/PolynomialTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+
 namespace PolynomialTests
 {
     [TestFixture]
@@ -7,8 +9,90 @@ namespace PolynomialTests
         [Test]
         public void PolynomialToString()
         {
-            Polynomial.Polynomial myPolinomial = new Polynomial.Polynomial(5,0,3,2,1);
-            Assert.AreEqual("5*X^4+3*X^2+2*X^1+1*X^0", myPolinomial.ToString());
+            Polynomial.Polynomial polinomialTest0 = new Polynomial.Polynomial(5, 0, 3, 2, 1);
+            Polynomial.Polynomial polinomialTest1 = new Polynomial.Polynomial();
+            Polynomial.Polynomial polinomialTest2 = new Polynomial.Polynomial(new double[0]);
+            Polynomial.Polynomial polinomialTest3 = new Polynomial.Polynomial(new double[] { 5, 0, 3, 2, 1 });
+            Polynomial.Polynomial polinomialTest4 = new Polynomial.Polynomial(6);
+            Polynomial.Polynomial polinomialTest5 = new Polynomial.Polynomial(5);
+            polinomialTest5[0] = 5;
+            polinomialTest5[1] = 0;
+            polinomialTest5[2] = 3;
+            polinomialTest5[3] = 2;
+            polinomialTest5[4] = 1;
+
+            Assert.AreEqual("5X^4+3X^2+2X+1", polinomialTest0.ToString());
+            Assert.AreEqual("", polinomialTest1.ToString());
+            Assert.AreEqual("", polinomialTest2.ToString());
+            Assert.AreEqual("5X^4+3X^2+2X+1", polinomialTest3.ToString());
+            Assert.AreEqual("", polinomialTest4.ToString());
+            Assert.AreEqual("5X^4+3X^2+2X+1", polinomialTest5.ToString());
         }
-    }
+        [Test]
+        public void PolynomialIndexCheck()
+        {
+            Polynomial.Polynomial polinomialTest0 = new Polynomial.Polynomial(5, 0, 3, 2, 1);
+            polinomialTest0[2] = 5;
+            Polynomial.Polynomial polinomialTest1 = new Polynomial.Polynomial(5);
+            polinomialTest1[2] = 5;
+
+            Assert.AreEqual("5X^4+5X^2+2X+1", polinomialTest0.ToString());
+            Assert.AreEqual("5X^2", polinomialTest1.ToString());
+        }
+        [Test]
+        public void PolynomialIndexArgumentOutOfRangeException()
+        {
+            Polynomial.Polynomial polinomialTest0 = new Polynomial.Polynomial(3);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => polinomialTest0[3] = 5);
+            Assert.Throws<ArgumentOutOfRangeException>(() => polinomialTest0[-1] = 5);
+        }
+        [Test]
+        public void PolynomialSumTwo()
+        {
+            Polynomial.Polynomial myPolinomial1 = new Polynomial.Polynomial(1, 2);
+            Polynomial.Polynomial myPolinomial2 = new Polynomial.Polynomial(3, 4, 5);
+            Polynomial.Polynomial myPolinomial3 = myPolinomial1 + myPolinomial2;
+
+            Polynomial.Polynomial myPolinomial4 = new Polynomial.Polynomial(-100, 2, 50, 0, 0, 2);
+            Polynomial.Polynomial myPolinomial5 = new Polynomial.Polynomial(-10, 3, 4, 0);
+            Polynomial.Polynomial myPolinomial6 = myPolinomial4 + myPolinomial5;
+
+            Assert.AreEqual("3X^2+5X+7", myPolinomial3.ToString());
+            Assert.AreEqual("-100X^5+2X^4+40X^3+3X^2+4X+2", myPolinomial6.ToString()); 
+        }
+        
+        [Test]
+        public void PolynomialSumTwoOverFlowException()
+        {
+            Polynomial.Polynomial myPolinomial1 = new Polynomial.Polynomial(Double.PositiveInfinity, 2);
+            Polynomial.Polynomial myPolinomial2 = new Polynomial.Polynomial(3, 1, 5);
+            Polynomial.Polynomial myPolinomial3;
+
+            Polynomial.Polynomial myPolinomial4 = new Polynomial.Polynomial(-100, 2, Double.NegativeInfinity, 0, 0, 2);
+            Polynomial.Polynomial myPolinomial5 = new Polynomial.Polynomial(-1, 3, 4, 0);
+            Polynomial.Polynomial myPolinomial6;
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => myPolinomial3 = myPolinomial1 + myPolinomial2);
+            Assert.Throws<ArgumentOutOfRangeException>(() => myPolinomial6 = myPolinomial4 + myPolinomial5); 
+        }
+            public void PolynomialDivTwo()
+            {
+
+                //Polynomial.Polynomial myPolinomial1 = new Polynomial.Polynomial(1, 2);
+                //Polynomial.Polynomial myPolinomial2 = new Polynomial.Polynomial(3, 0, 5);
+                //myPolinomial2[1] = 4;
+                //Polynomial.Polynomial myPolinomial3 = myPolinomial1 - myPolinomial2;
+                //Assert.AreEqual("-3*X^2-3*X^1-3*X^0", myPolinomial3.ToString());
+                //Polynomial.Polynomial myPolinomial1 = new Polynomial.Polynomial(1, 2, 3, 4);
+                //Polynomial.Polynomial myPolinomial2 = new Polynomial.Polynomial(3, 5);
+                //Polynomial.Polynomial myPolinomial3 = 2 * myPolinomial1;
+                //Assert.AreEqual("3*X^2+3*X^1+3*X^0", myPolinomial3.ToString());
+                //Polynomial.Polynomial myPolinomial1 = new Polynomial.Polynomial(1, 2, 3);
+                //Polynomial.Polynomial myPolinomial2 = new Polynomial.Polynomial(3, 5);
+                //Polynomial.Polynomial myPolinomial3 = myPolinomial2 * myPolinomial1;
+                //Assert.AreEqual("3X^3+11X^2+19X+15", myPolinomial3.ToString());
+            }
+        }
 }
+
